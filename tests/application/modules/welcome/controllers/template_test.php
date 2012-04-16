1180880b5138140a5ab4a9c5b2fdfc27489e4b1f
Maxim Medinskiy <maxwhite.nemetc@gmail.com>
2012-04-16 18:15:23 +0300

    Added test class for Template controller

---
+++ tests/application/modules/welcome/controllers/template_test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: An
+ * Date: 14.04.12
+ * Time: 23:21
+ * To change this template use File | Settings | File Templates.
+ */
+
+class template_test extends CI_TestCase
+{
+    public function test_index()
+    {
+         $this->assertTrue(true);
+    }
+
+}
